062f4e70afe62c3c01bfda7cd7271af44f0494ad
desktopbuddy <hanchondo21@gmail.com>
2023-09-08 15:51:54 -0700

    Create mySQL connection

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'F00l3yco0l3!',
+    database: 'employee_db',
+})

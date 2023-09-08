abe54ea01980ff1679325a6648b3863909e40f7e
desktopbuddy <hanchondo21@gmail.com>
2023-09-08 16:05:07 -0700

    Add first question

--- index.js
+++ index.js
@@ -7,3 +7,20 @@ const connection = mysql.createConnection({
     password: 'F00l3yco0l3!',
     database: 'employee_db',
 })
+
+const questions = [
+    {
+        type: 'list',
+        name: 'action',
+        message: 'What would you like to do?',
+        choices: [
+            'View all departments',
+            'View all roles',
+            'View all employees',
+            'Add a department',
+            'Add a role',
+            'Add an employee',
+            'Update an employee role',
+        ],
+    },
+];

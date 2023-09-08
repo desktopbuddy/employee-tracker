0e26890b8b015503a32fa2fc2dffe1c72036e451
desktopbuddy <hanchondo21@gmail.com>
2023-09-08 16:02:34 -0700

    Add employee table to db

--- db/schema.sql
+++ db/schema.sql
@@ -15,5 +15,22 @@ CREATE TABLE role {
     department_id INT NOT NULL,
     FOREIGN KEY (department_id)
     REFERENCES department(id)
-    ON DELETE CASCADE ON UPDATE CASCADE
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
 };
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE,
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON DELETE SET NULL
+    ON UPDATE CASCADE
+)
